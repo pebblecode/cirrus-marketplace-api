fdc53973d1c846744f0e83b67cbaa59445704dbf
Dave Hillier <dave.hillier1@gmail.com>
2016-06-01 10:13:04 +0100

    Added Cirrus Framework Migrations

---
+++ migrations/versions/600_add_cirrus_framework.py
@@ -0,0 +1,72 @@
+"""Add Cirrus
+
+Revision ID: 600_Cirrus
+Revises: 590
+Create Date: 2015-06-17 14:44:08.138355
+"""
+
+# revision identifiers, used by Alembic.
+revision = '600_Cirrus'
+down_revision = '590'
+
+from alembic import op
+from sqlalchemy.sql import table, column
+from sqlalchemy import String
+
+
+frameworks = table('frameworks',
+    column('name', String),
+    column('framework', String),
+    column('status', String),
+)
+
+
+def upgrade():
+    op.execute(
+        frameworks.insert().
+        values({'name': op.inline_literal('Cirrus Framework'),
+                'framework': op.inline_literal('cirrus'),
+                'slug': op.inline_literal('cirrus-1'),
+                'status': op.inline_literal('open')}))
+
+    lot_table = table(
+        'lots',
+        column('name', sa.String),
+        column('slug', sa.String),
+        column('one_service_limit', sa.Boolean)
+    )
+
+    op.bulk_insert(lot_table, [
+        {'name': 'Contingent Labour and Consultancy Services', 'slug': 'clcs', 'one_service_limit': False},
+        {'name': 'Catering Services', 'slug': 'catering', 'one_service_limit': False},
+    ])
+
+    conn = op.get_bind()
+    res = conn.execute("SELECT id FROM frameworks WHERE slug = 'cirrus-1'")
+    framework = list(res.fetchall())
+
+    res = conn.execute("SELECT id FROM lots WHERE slug in ('clcs', 'catering')")
+    lots = list(res.fetchall())
+
+    if len(framework) == 0:
+        raise Exception("Framework not found")
+
+    for lot in lots:
+        op.execute("INSERT INTO framework_lots (framework_id, lot_id) VALUES({}, {})".format(
+            framework[0]["id"], lot["id"]))
+
+
+def downgrade():
+    op.execute(
+        frameworks.delete().where(frameworks.c.name == 'Cirrus Framework'))
+    conn = op.get_bind()
+    res = conn.execute("SELECT id FROM frameworks WHERE slug = 'cirrus-1'")
+    framework = list(res.fetchall())
+
+    op.execute("""
+        DELETE FROM framework_lots WHERE framework_id={}
+    """.format(framework[0]['id']))
+
+    op.execute("""
+        DELETE from lots WHERE slug in ('clcs', 'catering');
+    """)
